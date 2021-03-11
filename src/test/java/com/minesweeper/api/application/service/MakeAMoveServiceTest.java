3c8d4a931d10cc99e3ce4b5f460fad3a1c97f4da
Joaquin Segui <segui.joaquin@gmail.com>
2021-03-11 02:42:08 -0300

    MakeAMove service happy path test

--- src/test/java/com/minesweeper/api/application/service/MakeAMoveServiceTest.java
+++ src/test/java/com/minesweeper/api/application/service/MakeAMoveServiceTest.java
@@ -1,12 +1,33 @@
 package com.minesweeper.api.application.service;
 
+import com.minesweeper.api.application.port.in.MakeAMoveCommand;
+import com.minesweeper.api.domain.Game;
+import com.minesweeper.api.objectMother.MakeAMoveObjectMother;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.junit.jupiter.MockitoExtension;
+import reactor.test.StepVerifier;
 
-import static org.junit.jupiter.api.Assertions.*;
-
+@ExtendWith(MockitoExtension.class)
 class MakeAMoveServiceTest {
+
+    private MakeAMoveService makeAMoveService;
+
+    @BeforeEach
+    void setUp() {
+        makeAMoveService = new MakeAMoveService();
+    }
 
     @Test
     void makeAMove() {
+        // GIVEN
+        Game game = Game.builder().id("123").rows("1").cols("2").mines("3").build();
+        MakeAMoveCommand command = MakeAMoveObjectMother.revealCommand();
+
+        // WHEN
+        StepVerifier.create(makeAMoveService.makeAMove(command))
+                // THEN
+                .expectNext(game).verifyComplete();
     }
 }
